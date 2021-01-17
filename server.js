848a8e7f3f6620258a9a56580bdfc0fd5e432014
Alexis Delaunay <alexis.f.delaunay@gmail.com>
2021-01-17 02:03:57 +0100

    asd

--- server.js
+++ server.js
@@ -16,12 +16,6 @@ app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 const server = http.createServer(app);
 server.listen(port, () => console.log(`App running on: http://localhost:${port}`));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    next();
-});
+
 
 
