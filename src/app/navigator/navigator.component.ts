af4bcdcc2857b9271d9c61d0a72417dd36cf3dd5
Alexis Delaunay <alexis.f.delaunay@gmail.com>
2021-01-20 03:04:25 +0100

    asd

--- src/app/navigator/navigator.component.ts
+++ src/app/navigator/navigator.component.ts
@@ -68,6 +68,7 @@ export class NavigatorComponent implements AfterViewInit {
       this.canvasRef.nativeElement.className = 'ksb'
     }
     console.log(this.currentArea.name)
+    this.updateLocation();
   }
 
   askForPermission() {
@@ -146,7 +147,7 @@ export class NavigatorComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
      this.askForPermission();
-    this.updateLocation();
+    
 
     this.ctx  = this.canvasRef.nativeElement.getContext('2d');
 
